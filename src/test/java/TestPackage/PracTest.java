47e6277e22c74963effb7e185ab82d3e2ee18e6a
DBForever-cloud <sudh.mrina@gmail.com>
2025-08-04 02:00:38 +0530

    Commit Two

--- src/test/java/TestPackage/PracTest.java
+++ src/test/java/TestPackage/PracTest.java
@@ -5,11 +5,12 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class PracTest {
-
-	public static void main(String[] args) {
-
+	
+	@Test
+	public void alertTest() {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
